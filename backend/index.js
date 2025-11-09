54984a36da8cd424a5af82459d4e3e806885f495
Jayam <jayamsogani03@gmail.com>
2025-11-09 12:25:08 +0530

    Fixed wildcard CORS route for Render compatibility

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options('*', cors());
+app.options(/.*/, cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
